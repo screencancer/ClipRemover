3da29b83ff4f4a6ae8382d492ac35038e95ce1de
twsmatt2 <twsmatt2@gmail.com>
2023-02-17 15:29:32 -0500

    Completed the logic.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.parser import parser
 
 
 def Setup():
@@ -15,7 +17,9 @@ def Setup():
     ExclusionList = [line.rstrip() for line in Exclusions]
     Exclusions.close()
 
-    return dir, FileSizeMax, ExclusionList
+    lastDate = datetime.datetime.now() - relativedelta(weeks=1)
+    formattedDate = lastDate.strftime("%m/%d/%Y")
+    return dir, FileSizeMax, ExclusionList, formattedDate
 
 
 #Get clips in directory
@@ -25,9 +29,10 @@ def getFiles(dirs):
     return list
 
 
-# Delete any clips older than 15 days old and larger than 200 MB. Exclude any thing set in exclusions file.
-def delFiles(files, FileSizeMax, ExclusionList, dirs):
+# Delete any clips older than the last date and larger than the MB set in the FileSizeMax.txt. Exclude any thing set in exclusions file.
+def delFiles(files, FileSizeMax, ExclusionList, dirs, lastdate):
     print(files)
+    lDate = datetime.datetime.strptime(lastdate,"%m/%d/%Y")
     for file in files:
         print(file)
         path = os.path.join(dirs, file)
@@ -35,17 +40,17 @@ def delFiles(files, FileSizeMax, ExclusionList, dirs):
         timecreated = os.path.getctime(path)
         size = os.path.getsize(path) >> 20
         date = datetime.datetime.fromtimestamp(timecreated).strftime("%m/%d/%Y")
+        parseddate = datetime.datetime.strptime(date, "%m/%d/%Y")
         print(f'{file} created on {date} and is {size} MB\n')
-        if date not in ExclusionList and size > FileSizeMax:
+        if date not in ExclusionList and size > FileSizeMax and parseddate <= lDate:
             print(f"Delete {file} {date} {size} MB")
+            os.remove(path)
 
 
-
-dirList, FileSizeMax, ExclusionList = Setup()
+dirList, FileSizeMax, ExclusionList, lastmonth = Setup()
 print(dir)
 print(FileSizeMax)
 print(ExclusionList)
-
 maxFile = int(FileSizeMax)
 files = getFiles(dirList)
-delFiles(files, maxFile, ExclusionList, dirList)
+delFiles(files, maxFile, ExclusionList, dirList, lastmonth)
